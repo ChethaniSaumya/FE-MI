d1ad9addfd40bf50b9c3bb3e8f84d8f410dd9812
Chethani Saumya <105449384+ChethaniSaumya@users.noreply.github.com>
2026-01-01 19:22:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx,mdx}',
+      './components/**/*.{js,ts,jsx,tsx,mdx}',
+      './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#EEB15C',
+          secondary: '#251721',
+        },
+              fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        galldis: ['Galldis', 'sans-serif'],
+      },
+      fontWeight: {
+        'galldis-medium': '500',
+        'galldis-bold': 'bold',
+      },
+        backgroundImage: {
+          'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+          'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        },
+        animation: {
+          'scroll': 'scroll 30s linear infinite',
+        },
+        keyframes: {
+          scroll: {
+            '0%': { transform: 'translateX(0)' },
+            '100%': { transform: 'translateX(-50%)' },
+          },
+        },
+      },
+    },
+    plugins: [],
+  };
+  
